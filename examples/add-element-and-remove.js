1eaba5b74a65f3aaaea2cbcb0cc68f4eb04131c0
Konstantin Petryaev <numminorihsf@gmail.com>
2017-10-24 10:15:52 +0700

    Update 2017-10-24T03:15:51.293Z

--- examples/add-element-and-remove.js
+++ examples/add-element-and-remove.js
@@ -1,3 +1,14 @@
+function modifySubtree(parent) {
+	const div = document.createElement('div');
+
+	div.style = [
+		'height: 50px',
+		'background: #d0'
+	].join(';');
+
+	parent.appendChild(div);
+}
+
 function run() {
 	const div = document.createElement('div');
 
@@ -7,6 +18,9 @@ function run() {
 	].join(';');
 
 	document.getElementById('target').appendChild(div);
+	setTimeout(function(){
+		modifySubtree(div);
+	}, 1000);
 	setTimeout(function(){
 		document.getElementById('target').removeChild(div);
 	}, 5000);
